6001b36d112739b4acd67d82c5016cc56a95f82a
Vikentios Vitalis <vitalisvikentios@gmail.com>
2023-04-18 15:11:22 +0300

    Delete SQL directory

--- SQL/Indexes.sql
+++
@@ -1,6 +0,0 @@
--- USE project2021; 
--- 3 Main indexes of our database
--- nfc_id / facility_id / service_id
-CREATE INDEX nfc_id_idx ON receiving_services(nfc_id);
-CREATE INDEX facility_id_idx ON having_access(facility_id);
-CREATE INDEX service_id_idx ON receiving_services(service_id);
